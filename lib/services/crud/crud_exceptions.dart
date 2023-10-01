f7b144cfa0cabbe8ae1102a0cbdbc0aaf271d631
Rishit Goel <rishit1114@gmail.com>
2023-10-01 18:57:43 +0530

    step-10'

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -7,3 +7,4 @@ class CouldNotFindUser implements Exception{}
 class CouldNotDeleteNote implements Exception{}
 class CouldNotFindNote implements Exception{}
 class CouldNotUpdateNote implements Exception{}
+class UserShouldBeSetBeforeReadingAllNotes implements Exception{}
--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:notes/extensions/list/filter.dart';
 import 'package:notes/services/crud/crud_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart' ;
@@ -14,6 +15,8 @@ class NotesService {
   Database? _db;
 
   List<DatabaseNote> _notes=[];
+  
+  DatabaseUser? _user;
 
   static final NotesService _shared = NotesService._sharedInstance();
   NotesService._sharedInstance(){
@@ -28,14 +31,30 @@ class NotesService {
   late final StreamController<List<DatabaseNote>> _notesStreamController;
   
 
-  Stream<List<DatabaseNote>> get allNotes=>_notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes=>_notesStreamController.stream.filter((note){
+    final currentUser=_user;
+    if(currentUser!=null){
+      return note.userID==currentUser.id;
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async{
+    }else{
+      throw UserShouldBeSetBeforeReadingAllNotes();
+    }
+  });
+
+  Future<DatabaseUser> getOrCreateUser({required String email,
+  bool setAsCurrentUser =true,}) async{
     try{
       final user = await getUser(email:email);
+      if(setAsCurrentUser){
+        _user=user;
+      }
       return user;
     } on CouldNotFindUser{
       final createdUser=await createUser(email: email);
+      if(setAsCurrentUser)
+      {
+        _user=createdUser;
+      }
       return createdUser;
     }catch(e)
     {
@@ -60,7 +79,8 @@ class NotesService {
     final updatesCount= await db.update(noteTable,{
       textColumn: text,
       isSyncColumn:0,
-    });
+    },where: 'id=?',
+    whereArgs: [note.id]);
 
     if (updatesCount==0)
     {
